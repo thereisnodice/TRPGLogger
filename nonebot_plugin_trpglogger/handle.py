b24ff75348ce2f115b4ee80241a12322e89dae61
jigsaw <j1g5aw@foxmail.com>
2022-02-23 22:45:42 +0800

    :pencil2: Fix a typo

--- nonebot_plugin_trpglogger/handle.py
+++ nonebot_plugin_trpglogger/handle.py
@@ -5,16 +5,16 @@ from .data import get_logger_time, is_logging, remove_logging, start_logging, st
 from .upload import upload_file
 
 
-def handle_command(**kargs):
-    if kargs["message"] == "on":
-        if is_logging(kargs["group_id"]):
+def handle_command(**kwargs):
+    if kwargs["message"] == "on":
+        if is_logging(kwargs["group_id"]):
             return "正在进行日志记录, 无法再次开始!"
         else:
-            start_logging(kargs["group_id"], kargs["time"])
+            start_logging(kwargs["group_id"], kwargs["time"])
             return "开始日志记录"
-    elif kargs["message"] == "off":
-        if is_logging(kargs["group_id"]):
-            stop_logging(kargs["group_id"])
+    elif kwargs["message"] == "off":
+        if is_logging(kwargs["group_id"]):
+            stop_logging(kwargs["group_id"])
             return "正在上传文件，请稍等"
         else:
             return "没有已开始的日志记录!"
@@ -24,28 +24,28 @@ def handle_command(**kargs):
         )
 
 
-def handle_logger(**kargs):
+def handle_logger(**kwargs):
     file_path = (
         Path()
         / "data"
         / "trpglogger"
-        / f"group_{kargs['group_id']}_{get_logger_time(kargs['group_id'])}.txt"
+        / f"group_{kwargs['group_id']}_{get_logger_time(kwargs['group_id'])}.txt"
     )
 
-    if is_logging(kargs["group_id"]):
+    if is_logging(kwargs["group_id"]):
         with file_path.open("a+", encoding="utf-8") as f:
             f.write(
-                f'{kargs["nickname"]}({kargs["user_id"]}) {time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(kargs["time"]))}\n'
+                f'{kwargs["nickname"]}({kwargs["user_id"]}) {time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(kwargs["time"]))}\n'
             )
-            f.write(f'{kargs["message"]}\n\n')
-        if kargs["time"] - get_logger_time(kargs["group_id"]) > 60 * 60 * 24:
-            stop_logging(kargs["group_id"])
+            f.write(f'{kwargs["message"]}\n\n')
+        if kwargs["time"] - get_logger_time(kwargs["group_id"]) > 60 * 60 * 24:
+            stop_logging(kwargs["group_id"])
             return "已持续记录24小时，已自动停止记录并上传"
     else:
         if file_path.exists():
             upload_file(str(file_path), "dicelogger", file_path.name)
             file_path.unlink()
-            remove_logging(kargs["group_id"])
+            remove_logging(kwargs["group_id"])
             return f"上传已完成，请访问 https://logpainter.kokona.tech/?s3={file_path.name} 以查看记录"
         else:
-            remove_logging(kargs["group_id"])
+            remove_logging(kwargs["group_id"])
